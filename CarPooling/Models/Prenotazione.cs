48d1c12fecc5c8efa56cadb20baa48bf44dcfd0e
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2024-12-10 15:59:51 +0100

    Update Prenotazione.cs

--- CarPooling/Models/Prenotazione.cs
+++ CarPooling/Models/Prenotazione.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace CarPooling.Models
 {
-    public class Prenotazione
+    public class Prenotazione : DatabaseObject
     {
+        public int IdPrenotazione { get; set; }
+        public string Emailpasseggero { get; set; }
+        public int IdViaggio { get; set; }
+        public DateTime DataOra { get; set; }
+
+        public void BuildFromReader(SqlDataReader reader)
+        {
+            
+        }
     }
 }
