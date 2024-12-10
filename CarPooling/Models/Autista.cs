a046b65e382de830d5bb21923a491135c732b923
MeryemeBanani <162060219+MeryemeBanani@users.noreply.github.com>
2024-12-10 17:02:17 +0100

    Ho fatto le due classi Passeggero e Autista

--- CarPooling/Models/Autista.cs
+++ CarPooling/Models/Autista.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 
+
 namespace CarPooling.Models
 {
     public class Autista : DatabaseObject
@@ -12,10 +13,25 @@ namespace CarPooling.Models
         public string EmailAutista {  get; set; }
         public string Nome {  get; set; }
         public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public int NumPatente { get; set; }
+        public DateTime scadenzaPatente { get; set; }
+        public string Auto { get; set; }
+        public string Tel { get; set; }
+        public string PhotoFileName { get; set; }
+        public HttpPostedFileBase File { get; set; }
+        
         public void BuildFromReader(SqlDataReader reader)
         {
-            //EmailAutista = reader["emailAutista"].ToString();
-            //...
+            EmailAutista = reader["emailAutista"].ToString();
+            Nome = reader["Nome"].ToString();
+            Cognome = reader["Cognome"].ToString();
+            DataNascita = (DateTime)(reader["DataNascita"].ToString());
+            NumPatente = int.Parse(reader["NumPatente"].ToString());
+            scadenzaPatente = (DateTime)(reader["ScadenzaPatente"].ToString());
+            Auto = reader["Auto"].ToString();
+            Tel = reader["Telefono"].ToString();
+            PhotoFileName = reader["PhotoFileName"].ToString();
         }
 
         public static List<Autista> SelectAllAutisti()
@@ -26,9 +42,40 @@ namespace CarPooling.Models
 
         public static Autista SelectById(string email)
         {
-            SqlCommand cmd = new SqlCommand("SELECT * FROM Autista WHERE emailAutista = @email");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Autista WHERE EmailAutista = @email");
             cmd.Parameters.AddWithValue("email", email);
             return Database.GetObject<Autista>(cmd);
         }
+
+        public static void InsertAutista(Autista autista)
+        {
+            string query = "INSERT INTO Autista VALUES (@EmailAutista, @Nome, @Cognome);";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("EmailAutista", autista.EmailAutista);
+            cmd.Parameters.AddWithValue("Nome", autista.Nome);
+            cmd.Parameters.AddWithValue("Cognome", autista.Cognome);
+            cmd.Parameters.AddWithValue("DataNascita", autista.DataNascita);
+            cmd.Parameters.AddWithValue("NumPatente", autista.NumPatente);
+            cmd.Parameters.AddWithValue("ScadenzaPatente", autista.scadenzaPatente);
+            cmd.Parameters.AddWithValue("Auto", autista.Auto);
+            cmd.Parameters.AddWithValue("Telefono", autista.Tel);
+            cmd.Parameters.AddWithValue("PhotoFileName", autista.PhotoFileName);
+
+            try
+            {
+                ExecuteNonQuery(cmd);
+                    
+            }
+            catch(Exception ex)
+            {
+                ViewBag.ErroreInsert = "Errore inserimento Passeggero" + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }   
+
+
     }
 }
--- CarPooling/Models/Passeggero.cs
+++ CarPooling/Models/Passeggero.cs
@@ -1,11 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 
 namespace CarPooling.Models
 {
-    public class Passeggero
+    public class Passeggero : DatabaseObject
     {
+        public string EmailPasseggero { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string CartaIdentita { get; set; }
+        public string Telefono { get; set; }
+       
+
+
+        public void BuildFromReader(SqlDataReader reader)
+        {
+            EmailPasseggero = reader["EmailPasseggero"].ToString();
+            Nome = reader["Nome"].ToString();
+            Cognome = reader["Cognome"].ToString();
+            CartaIdentita = reader["CartaIdentita"].ToString();
+            Telefono = reader["Telefono"].ToString();
+        }
+
+        public static List<Passeggero> SelectAllPasseggeri()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Passeggero;");
+            return Database.GetObjectList<Passeggero>(cmd);
+        }
+
+        public static Passeggero SelectById(string email)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Passeggero WHERE EmailPasseggero= @email;");
+            cmd.Parameters.AddWithValue("email", email);
+            return Database.GetObject<Passeggero>(cmd);
+        }
+
+        public static void InsertPasseggero(Passeggero p)
+        {
+            string query = "INSERT INTO Passeggero VALUES (@EmailPasseggero, @Nome, @Cognome, @CartaIdentita, @Telefono);";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("EmailPasseggero", p.EmailAutista);
+            cmd.Parameters.AddWithValue("Nome", p.Nome);
+            cmd.Parameters.AddWithValue("Cognome", p.Cognome);
+            cmd.Parameters.AddWithValue("CartaIdentita", p.Cognome);
+            cmd.Parameters.AddWithValue("Telefono", p.Cognome);
+            
+
+            try
+            {
+                ExecuteNonQuery(cmd);
+
+            }
+            catch (Exception ex)
+            {
+                   ViewBag.ErroreInsert = "Errore inserimento Passeggero" +ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
     }
 }
