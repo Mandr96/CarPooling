9b926faebbb62b9c4d9177839e6a482b0959001e
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2024-12-11 09:38:11 +0100

    PasseggeriController

---
+++ CarPooling/Controllers/PasseggeriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarPooling.Controllers
+{
+    public class PasseggeriController : Controller
+    {
+        // GET: Passeggeri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
