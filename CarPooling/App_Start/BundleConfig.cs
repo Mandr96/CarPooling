bf0cc8f5a2af01e753f16400a75a6086ce19547a
Raffaele <raffaele96ma@hotmail.it>
2024-12-13 14:50:44 +0100

    Formattata la data in mostra prenotazioni in HomePasseggero, aggiunta la funzione dateconvert al bundle di script, layout, ecc

--- CarPooling/App_Start/BundleConfig.cs
+++ CarPooling/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace CarPooling
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new Bundle("~/bundles/customizeJS").Include(
+                    "~/Scripts/DateConvertFromServer.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ CarPooling/Scripts/DateConvertFromServer.js
@@ -0,0 +1,11 @@
+﻿
+function DateJs(DataDaConvertire) {
+    var dataJson = DataDaConvertire;
+
+    let timeSpan = parseInt(dataJson.match(/\d+/));
+
+    let dataToReturn = new Date(timeSpan);
+
+    let DateAndTime = dataToReturn.toLocaleDateString() + ", " + dataToReturn.toLocaleTimeString();
+    return DateAndTime;
+}
